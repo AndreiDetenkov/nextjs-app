49adab924fde53ef8ec93118975548bd1c98ecd9
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-03-07 14:01:03 +0600

    feat: styling footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,17 @@
 import {DetailedHTMLProps, HTMLAttributes} from "react";
-
+import cn from "classnames";
+import {format} from "date-fns";
+import styles from './Footer.module.css';
 interface FooterProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>{ }
 
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
   return (
-      <div {...props}>
-        Footer
-      </div>
+      <footer className={cn(className, styles.footer)} {...props}>
+        <div>
+          TopApp @ 2020 - {format(new Date(), 'yyyy')} Все права защищены
+        </div>
+        <a href="#" target="_blank" rel="noreferrer noopener">Пользовательское соглашение</a>
+        <a href="#" target="_blank" rel="noreferrer noopener">Политика конфиденциальности</a>
+      </footer>
   );
 };
